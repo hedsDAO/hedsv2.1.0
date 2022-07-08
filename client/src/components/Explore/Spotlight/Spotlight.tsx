a2f04ddc437f5b64fa47ce6697209308c9e68933
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2022-07-08 16:15:38 -0700

    fixed tape link route and updated styling for tape image

--- client/src/components/Explore/Spotlight/Spotlight.tsx
+++ client/src/components/Explore/Spotlight/Spotlight.tsx
@@ -9,10 +9,10 @@ interface SpotlightProps {
 	exploreData: GlobalState;
 }
 const Spotlight: React.FC<SpotlightProps> = ({ featured, exploreData }: SpotlightProps) => {
-	const { space, tape, id } = exploreData.spotlight;
+	const { space, tape } = exploreData.spotlight;
 	return (
-		<div className="col-span-12 bg-neutral-950 border-[0.25px] border-neutral-800 rounded-lg max-w-[100rem] mx-2 xl:mx-auto my-2 xl:my-5">
-			<div className="lg:z-10 lg:relative py-8 lg:py-10 border-[0.25px] border-neutral-800 2xl:mx-auto m-1 spotlight-gradient max-w-[100rem] rounded-md">
+		<div className="col-span-12 bg-neutral-950 border-[0.25px] border-neutral-800 rounded-lg max-w-[110rem] p-2 mx-2 xl:mx-auto xl:my-5">
+			<div className="lg:z-10 lg:relative py-8 lg:py-10 border-[0.25px] border-neutral-800 2xl:mx-auto spotlight-gradient max-w-[110rem] rounded-md">
 				<div className="mx-auto max-w-4xl px-4 py-4 rounded-lg">
 					<div className="grid lg:grid-cols-2 items-center gap-x-4 place-content-center">
 						<div className="lg:col-span-1 flex flex-col items-center lg:items-end lg:justify-end">
@@ -25,23 +25,23 @@ const Spotlight: React.FC<SpotlightProps> = ({ featured, exploreData }: Spotligh
 									<img src={HedDot} className="h-12 lg:h-20 inline-block rounded-full bg-neutral-900 p-2 xl:p-3" />
 								</div>
 								<div className="inline-flex items-center">
-									<span className="text-neutral-300 font-serif text-xs mt-2 tracking-widest mb-3 whitespace-nowrap pr-1 py-1 mr-0.5">
+									<span className="text-neutral-300 font-serif text-xs mt-2 mb-1 tracking-widest whitespace-nowrap pr-1 py-1 mr-0.5">
 										heds
 									</span>
-									<span className="text-neutral-300 font-serif text-xs mt-2 tracking-widest mb-3 whitespace-nowrap pr-1 py-1 mr-0.5">
+									<span className="text-neutral-300 font-serif text-xs mt-2 mb-1 tracking-widest whitespace-nowrap pr-1 py-1 mr-0.5">
 										/
 									</span>
-									<span className="text-neutral-300 font-serif text-xs mt-2 tracking-widest mb-3 whitespace-nowrap pr-1 py-1 mr-0.5">
+									<span className="text-neutral-300 font-serif text-xs mt-2 mb-1 tracking-widest whitespace-nowrap pr-1 py-1 mr-0.5">
 										hedTAPE
 									</span>
-									<span className="text-neutral-300 font-serif text-xs mt-2 tracking-widest mb-3 whitespace-nowrap pr-1 py-1 mr-0.5">
+									<span className="text-neutral-300 font-serif text-xs mt-2 mb-1 tracking-widest whitespace-nowrap pr-1 py-1 mr-0.5">
 										/
 									</span>
-									<span className="text-neutral-300 font-serif text-xs mt-2 tracking-widest mb-3 whitespace-nowrap pr-1 py-1 mr-0.5">
+									<span className="text-neutral-300 font-serif text-xs mt-2 mb-1 tracking-widest whitespace-nowrap pr-1 py-1 mr-0.5">
 										5
 									</span>
 								</div>
-								<span className="text-neutral-100 text-2xl uppercase tracking-widest mb-1 whitespace-nowrap">
+								<span className="text-neutral-100 text-2xl uppercase tracking-widest mb-4 whitespace-nowrap">
 									{featured?.sample?.artist} <span className="lowercase">x</span> Heds
 								</span>
 								<span className="text-neutral-200 mb-6 font-thin text-sm tracking-widest lg:text-left text-center">
