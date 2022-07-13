6612685a00a4bab03ea51def2738b95a60111316
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2022-07-13 11:43:12 -0700

    updated explore artists

--- client/src/components/Explore/Artists/Artists.tsx
+++ client/src/components/Explore/Artists/Artists.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { Dispatch, RootState } from "../../../store";
 
 const Artists = () => {
@@ -9,22 +10,34 @@ const Artists = () => {
 		dispatch.audioModel.getSamples();
 	}, []);
 	return (
-		<div className="max-w-7xl w-full mx-auto mt-20">
-			<ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 place-items-center">
-				{audioData?.samples &&
-					audioData?.samples.map((sample) => (
-						<li className="col-span-1 flex flex-col text-center bg-neutral-950 rounded-lg shadow divide-y divide-neutral-700 border-[0.25px] border-neutral-700">
-							<div className="flex-1 flex flex-col">
-								<img className="w-32 h-32 flex-shrink-0 mx-auto rounded-lg" src={sample.image} alt="" />
-								<h3 className="mt-6 text-neutral-500 text-sm font-medium">{sample.artist}</h3>
-								<dl className="mt-1 flex-grow flex flex-col justify-between">
-									<dt className="sr-only">wallet</dt>
-									<dd className="text-gray-500 text-sm">{sample.wallet.slice(0, 5) + "..."}</dd>
-								</dl>
-							</div>
-						</li>
-					))}
-			</ul>
+		<div className="max-w-7xl w-full mx-auto my-20">
+			<div className="xl:max-w-7xl flex lg:flex-row flex-col justify-end mx-auto items-center pb-5 lg:px-1 px-6 gap-y-4 mt-10 xl:mt-20">
+				<div className="xl:mx-0 mx-2 xl:my-0 my-5 text-right">
+					<h1 className="text-4xl tracking-wide font-extrabold text-neutral-300">CURATORS</h1>
+					<p className="mt-3 text-sm lg:text-base text-neutral-500 xl:whitespace-nowrap xl:max-w-full max-w-sm">
+						The tapes are built from an artist sample. View the roster of talented artists and the samples provided.
+					</p>
+				</div>
+			</div>
+			<div className="col-span-12 bg-neutral-950 border-[0.25px] border-neutral-800 rounded-lg p-2">
+				<ul role="list" className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 place-items-center">
+					{audioData?.samples &&
+						audioData?.samples.map((sample, index: number) => (
+							<li className="col-span-1 flex flex-col text-center rounded-lg hover:bg-neutral-900">
+								<Link to={`/listen/heds/hedstape/${index + 1}`}>
+									<div className="flex-1 flex flex-col items-start p-2">
+										<img className="lg:w-full lg:h-full flex-shrink-0 mx-auto rounded-lg" src={sample.image} alt="" />
+										<h3 className="mt-4 text-neutral-500 text-sm font-medium">{sample.artist}</h3>
+										<dl className="mt-1 flex-grow flex flex-col justify-between">
+											<dd className="text-gray-500 text-sm">{sample.wallet.slice(0, 5) + "..."}</dd>
+										</dl>
+										<span className="text-gray-500 text-xs mt-1">{`hedsTAPE 0${index + 1}`}</span>
+									</div>
+								</Link>
+							</li>
+						))}
+				</ul>
+			</div>
 		</div>
 	);
 };
