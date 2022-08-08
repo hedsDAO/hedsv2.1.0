e841fddb034c818a00c5adf6bfe42186ae52c270
Ramtin Khoee <ramtinkhoee@Ramtins-MacBook-Pro.local>
2022-08-08 12:29:07 -0700

    updated possible fix for VP

--- client/src/utils/parseAddresses.ts
+++ client/src/utils/parseAddresses.ts
@@ -1,12 +1,12 @@
 import { CollectionItem, CollectionTank } from "../models/common";
 
 const hedsTapeAddresses: Array<string> = [
-	"0xde8a0b17d3dc0468adc65309881d9d6a6cd66372",
-	"0x5083cf11003f2b25ca7456717e6dc980545002e5",
-	"0x567e687c93103010962f9e9cf5730ae8dbfc6d41",
-	"0x8045fd700946a00436923f37d08f280ade3b4af6",
-	"0x8f36eB094F7B960a234a482d4d8FFb8b37f728C6",
-	"0x885236535d5cf7033bdc5bc1050cad7fdf4970a6"
+	"0xde8a0b17d3dc0468adc65309881d9d6a6cd66372".toLowerCase(),
+	"0x5083cf11003f2b25ca7456717e6dc980545002e5".toLowerCase(),
+	"0x567e687c93103010962f9e9cf5730ae8dbfc6d41".toLowerCase(),
+	"0x8045fd700946a00436923f37d08f280ade3b4af6".toLowerCase(),
+	"0x8f36eB094F7B960a234a482d4d8FFb8b37f728C6".toLowerCase(),
+	"0x885236535d5cf7033bdc5bc1050cad7fdf4970a6".toLowerCase()
 ];
 
 const parseAddresses = (userCollection: any) => {
@@ -15,7 +15,7 @@ const parseAddresses = (userCollection: any) => {
 	if (Array.isArray(userCollection))
 		userCollection.map((item: CollectionItem) => {
 			const { token_address, token_uri, name, metadata } = item;
-			if (hedsTapeAddresses.includes(item.token_address)) {
+			if (hedsTapeAddresses.includes(item.token_address.toLowerCase())) {
 				if (collectionTank?.[token_address]?.quantity) {
 					collectionTank[token_address].quantity++;
 				} else collectionTank[token_address] = { quantity: 1, token_address, token_uri, name, metadata };
