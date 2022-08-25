fb0d722613c68014aee541e2ef6e480afcc651a0
Ramtin Khoee <ramtinkhoee@Ramtins-MacBook-Pro.local>
2022-08-25 13:50:05 -0700

    updated claimed functionality

--- client/src/common/modal/PreMintModal/PreMintModal.tsx
+++ client/src/common/modal/PreMintModal/PreMintModal.tsx
@@ -8,54 +8,57 @@ const contractAbi = require("../../../data/whitelists/abi/collabTAPE01.json");
 const proof = require("../../../data/whitelists/proofs.json");
 var ethers = require("ethers");
 
-
 const PreMintModal = () => {
     const { web3, enableWeb3, isWeb3Enabled, user } = useMoralis();
     const { id, tape } = useSelector((state: RootState) => state.globalModel);
-    // @ts-ignore
     const [hasMinted, setHasMinted] = useState<boolean>(false);
     const [isMinting, setIsMinting] = useState<boolean>(false);
     const [isLoading, setIsLoading] = useState<boolean>(false);
-    // @ts-ignore
+    const [hasClaimed, setHasClaimed] = useState<boolean>(false);
     const [error, setError] = useState<string>("");
     const currentTape = useSelector((state: RootState) => state.tapeModel).tapes?.[tape]?.[id];
     const { locked, open } = useSelector((state: RootState) => state.globalModel.modal);
     const dispatch = useDispatch<Dispatch>();
+
     const handleMint = async () => {
-        setError("")
+        setError("");
         setIsLoading(true);
-        console.log()
         const userProof = proof?.[user?.attributes?.ethAddress];
-        console.log(userProof)
-        console.log(userProof);
-        if (web3) {
-            // const contractAbi = await getContractAbi(currentTape.tape.contract);
-            // const formattedAbi = JSON.parse(contractAbi.data.result);
-            const contract = new ethers.Contract("0xfb30153A13217815C08a1Ad26EAdAe5723116a14", contractAbi, web3.getSigner());
-            try {
-                setIsMinting(true)
-                const claimed  = await contract.claimed();
-                if (claimed) {
-                    setHasMinted(true);
-                } else {
+        if (web3 && user) {
+            const wallet = user?.attributes?.ethAddress;
+            const contract = new ethers.Contract(
+                "0xfb30153A13217815C08a1Ad26EAdAe5723116a14",
+                contractAbi,
+                web3.getSigner()
+            );
+            const claimed = await contract.claimed(wallet);
+            if (claimed) {
+                setHasClaimed(true);
+                setIsLoading(false);
+            } else {
+                try {
+                    setIsMinting(true);
                     const txn = await contract.preMint(userProof);
                     const receipt = await txn.wait();
                     console.log(receipt);
                     setHasMinted(true);
+                } catch (err: any) {
+                    setIsMinting(false);
+                    console.log(err);
+                    if (err?.message?.includes("insufficient funds")) {
+                        setError("Insufficient funds for minting. Please try again.");
+                    } else setError("There was a problem minting your tapes. Please try again.");
                 }
-            } catch (err: any) {
-                setIsMinting(false)
-                console.log(err);
-                if (err?.message?.includes("insufficient funds")) {
-                    setError("Insufficient funds for minting. Please try again.");
-                } else setError("There was a problem minting your tapes. Please try again.");
             }
         }
         setIsLoading(false);
     };
     return (
         <Transition appear show={open} as={Fragment}>
-            <Dialog as="div" className="relative z-[60]" onClose={locked ? () => { } : () => dispatch.globalModel.setModalVisibility(false)}>
+            <Dialog
+                as="div"
+                className="relative z-[60]"
+                onClose={locked ? () => {} : () => dispatch.globalModel.setModalVisibility(false)}>
                 <div className="fixed inset-0 overflow-y-auto">
                     <div className="flex bg-neutral-950/90 min-h-full items-center justify-center text-center">
                         <Transition.Child
@@ -68,45 +71,80 @@ const PreMintModal = () => {
                             leaveTo="opacity-0 scale-95">
                             <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-lg align-middle transition-all mt-5">
                                 <div className="relative z-50 inline-block align-bottom bg-neutral-950 border-[0.25px] border-neutral-700 rounded-lg py-4 lg:px-5 px-20 text-left overflow-hidden shadow-xl transform transition-all sm:align-middle max-w-full sm:max-w-md sm:w-full">
-                                    {!hasMinted ? <div className="flex flex-col h-full items-center justify-center py-5">
-                                        <h5 className="text-base font-semibold text-gray-200 lg:text-xl text-center py-5">
-                                            {currentTape?.tape?.name}
-                                        </h5>
-                                        <span className="uppercase font-normal ml-2 text-neutral-400">Whitelist Mint</span>
-                                        <div className="mb-2">
-                                            <div className="mt-5 flex justify-center flex-col gap-y-3">
-                                                <div className="mx-auto self-center mb-10 py-5 max-w-[10rem] max-h-[10rem]">
-                                                    <img
-                                                        className="min-h-[10rem] min-w-[10rem] object-cover border-[0.25px] border-neutral-700"
-                                                        src={currentTape?.tape?.image}
-                                                        alt="submission tape image"
-                                                    />
+                                    {hasClaimed ? (
+                                        <div className="flex flex-col items-center gap-y-1 py-3">
+                                            <h5 className="text-base font-semibold text-gray-200 lg:text-xl text-center">
+                                                {currentTape?.tape?.name}
+                                            </h5>
+                                            <span className="uppercase font-normal ml-2 text-neutral-400">
+                                                CLAIMED{" "}
+                                                <i className="fa-solid fa-circle-check text-green-500 ml-1"></i>
+                                            </span>
+                                        </div>
+                                    ) : !hasMinted ? (
+                                        <div className="flex flex-col h-full items-center justify-center py-3">
+                                            <h5 className="text-base font-semibold text-gray-200 lg:text-xl text-center py-5">
+                                                {currentTape?.tape?.name}
+                                            </h5>
+                                            <span className="uppercase font-normal ml-2 text-neutral-400">
+                                                Whitelist Mint
+                                            </span>
+                                            <div className="mb-2">
+                                                <div className="mt-5 flex justify-center flex-col gap-y-3">
+                                                    <div className="mx-auto self-center mb-10 py-5 max-w-[10rem] max-h-[10rem]">
+                                                        <img
+                                                            className="min-h-[10rem] min-w-[10rem] object-cover border-[0.25px] border-neutral-700"
+                                                            src={currentTape?.tape?.image}
+                                                            alt="submission tape image"
+                                                        />
+                                                    </div>
                                                 </div>
                                             </div>
+                                            {error && (
+                                                <span className="text-xs uppercase font-semibold mb-2 text-red-500 text-center">
+                                                    {error}
+                                                </span>
+                                            )}
+                                            <div className="gap-x-2 flex justify-center items-stretch pt-4 mt-5">
+                                                {isMinting ? (
+                                                    <LoadingIcon />
+                                                ) : (
+                                                    <Fragment>
+                                                        <button
+                                                            disabled={isLoading}
+                                                            onClick={() =>
+                                                                dispatch.globalModel.setModalVisibility(
+                                                                    false
+                                                                )
+                                                            }
+                                                            className="px-4 py-1 text-sm bg-neutral-850 text-neutral-400 font-thin inline-flex items-center rounded-sm focus:outline-none hover:bg-neutral-800 transition-all">
+                                                            BACK
+                                                        </button>
+                                                        <button
+                                                            disabled={isLoading ? true : false}
+                                                            onClick={
+                                                                isWeb3Enabled
+                                                                    ? () => handleMint()
+                                                                    : () => enableWeb3()
+                                                            }
+                                                            className="px-4 py-1 text-sm bg-green-900 hover:bg-green-800 text-neutral-400 font-thin inline-flex items-center rounded-sm focus:outline-none disabled:bg-neutral-700">
+                                                            {isWeb3Enabled ? "MINT" : "CONNECT"}
+                                                        </button>
+                                                    </Fragment>
+                                                )}
+                                            </div>
                                         </div>
-                                        <span className="uppercase text-sm ml-2 text-neutral-500">Quantity: 1</span>
-                                        {error && <span className="text-xs uppercase font-semibold mb-2 text-red-500 text-center">{error}</span>}
-                                        <div className="gap-x-2 flex justify-center items-stretch pt-4 mt-5">
-                                            {isMinting ?
-                                                <LoadingIcon /> :
-                                                <Fragment>
-                                                    <button
-                                                        disabled={isLoading}
-                                                        onClick={() => dispatch.globalModel.setModalVisibility(false)}
-                                                        className="px-4 py-1 text-sm bg-neutral-850 text-neutral-400 font-thin inline-flex items-center rounded-sm focus:outline-none hover:bg-neutral-800 transition-all">
-                                                        BACK
-                                                    </button>
-                                                    <button
-                                                        disabled={isLoading ? true : false}
-                                                        onClick={isWeb3Enabled ? () => handleMint() : () => enableWeb3()}
-                                                        className="px-4 py-1 text-sm bg-green-900 hover:bg-green-800 text-neutral-400 font-thin inline-flex items-center rounded-sm focus:outline-none disabled:bg-neutral-700">
-                                                        {isWeb3Enabled ? "MINT" : "CONNECT"}
-                                                    </button></Fragment>}
+                                    ) : (
+                                        <div className="flex flex-col items-center gap-y-1 py-3">
+                                            <h5 className="text-base font-semibold text-gray-200 lg:text-xl text-center">
+                                                {currentTape?.tape?.name}
+                                            </h5>
+                                            <span className="uppercase font-normal ml-2 text-neutral-400">
+                                                MINTED{" "}
+                                                <i className="fa-solid fa-circle-check text-green-500 ml-1"></i>
+                                            </span>
                                         </div>
-                                    </div> : <h5 className="text-base font-semibold text-gray-200 lg:text-xl text-center py-5">
-                                        {currentTape?.tape?.name}
-                                        <span className="uppercase font-normal ml-2 text-neutral-400">Minted <i className="fa-solid fa-circle-check text-green-500 ml-1"></i></span>
-                                    </h5>}
+                                    )}
                                 </div>
                             </Dialog.Panel>
                         </Transition.Child>
