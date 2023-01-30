63c8b9a8f5b7eabeb40e7c766c533a756153c717
Ramtin Khoee <71619815+Rahmteen@users.noreply.github.com>
2023-01-30 15:00:57 -0800

    update dark/light mode text

--- client/src/pages/Loading/Loading.tsx
+++ client/src/pages/Loading/Loading.tsx
@@ -10,13 +10,13 @@ const Loading = () => {
         <div className="min-h-[100vh] min-w-screen flex items-center justify-center">
             {isUnderConstruction && (
                 <div className="flex flex-col items-center justify-center gap-2">
-                    <div className="mb-4 text-white font-thin flex items-center gap-2">
+                    <div className="mb-4 text-neutral-900 dark:text-neutral-300 font-thin flex items-center gap-2">
                         <span className="text-sm">heds v2.1.0</span>
                         <i className="fa-light fa-arrow-right-long text-xs"></i>
                         <span className="text-sm">v3.0.0</span>
                     </div>
-                    <i className="fas fa-circle-notch fa-spin text-white text-3xl"></i>
-                    <p className="text-white font-thin mt-4">
+                    <i className="fas fa-circle-notch fa-spin text-neutral-900 dark:text-neutral-300 text-3xl"></i>
+                    <p className="text-neutral-900 dark:text-neutral-300 font-thin mt-4">
                         under construction
                         <span className="one">.</span>
                         <span className="two">.</span>
